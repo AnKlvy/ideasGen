d3697ea7aa7db8a1616101aa3b61ef3f3f5d4ee4
AnKlvy <andreyklimov.ver2@gmail.com>
2023-03-09 06:32:10 +0600

    add bootstrap home page

--- ideasGen/urls.py
+++ ideasGen/urls.py
@@ -1,4 +1,4 @@
-"""ideasGen URL Configuration
+"""ideaGen URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ideas.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home)
+
 ]
